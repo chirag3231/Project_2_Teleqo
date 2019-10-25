10a639e4a6feb17f75a68aaa6a5dd2cd0f57f4ce
VijayKumar02 <56170897+VijayKumar02@users.noreply.github.com>
2019-10-25 12:18:31 -0700

    Add files via upload

---
+++ Footer.jsx
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import logofooter from './logofooter.png';
+import './style.css';
+
+class Footer extends Component {
+    render() {
+  return (
+    <footer>
+      <div><img src={logofooter} class="logofooter" alt="logo" /></div>
+      <div><nav>
+            <ul class="navbar">
+            <li><a href="home.jsx">Home</a></li>
+                <li><a href="coverage.jsx">Coverage</a></li>	
+                <li><a href="about.jsx">About us</a></li>
+                <li><a href="contact.jsx">Contact us</a></li>
+            </ul>
+        </nav>
+        </div>
+        <div id="copy">&copy;2019</div>
+    </footer>
+  );
+}
+}
+
+export default Footer;
